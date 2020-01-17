1e3c0d5e815747311a7c77dd6b02107ea95e3a1e
Your Name <sem16002@byui.edu>
2020-01-17 12:32:08 -0700

    added assign_02

---
+++ web/week02/assign_02/assign02.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>| This is Awesome</title>
+</head>
+
+<body>
+    <form class="grid-container">
+        <div class="item1">
+            <?php
+            echo "Today is " . date("l") . date("d/m/Y");
+            ?>
+        </div>
+    </form>
+</body>
+
+</html>
