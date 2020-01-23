d2dcd7a84228e557e2c4375a27f0a3e5f25d9e3c
Your Name <sem16002@byui.edu>
2020-01-23 16:02:10 -0700

    class cookie practice again again and again

--- web/week03/class_practice/thursdayClassTemp/thursdaySession1.php
+++ web/week03/class_practice/thursdayClassTemp/thursdaySession1.php
@@ -1,16 +1,24 @@
 <?php
-   // Start the session
+// Start the session
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
-   <body>
-      <?php
-         // remove previous session variable
-         // Set session variables
-         // echo that variables have been set
-?>
-      <a href="thursdaySession2.php">Check the variables on another page</a>
 
-      <?php // set session variables using a form ?>
-   </body>
+<body>
+   <?php
+   // remove previous session variable
+
+   // Set session variables
+   $_SESSION["favcolor"] = "green";
+   $_SESSION["favanimal"] = "dolphin";
+   // echo that variables have been set
+   echo "Session variables have been set.";
+   ?>
+   <a href="thursdaySession2.php">Check the variables on another page</a>
+
+   <?php // set session variables using a form 
+   ?>
+</body>
+
 </html>
