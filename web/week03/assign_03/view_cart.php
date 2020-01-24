0508c5beeaed2568147e878f430e5d641966214b
Your Name <sem16002@byui.edu>
2020-01-23 22:06:34 -0700

    assign03 commit number 2

--- web/week03/assign_03/view_cart.php
+++ web/week03/assign_03/view_cart.php
@@ -35,7 +35,7 @@
             <?php
             $choice = $_GET['choice'];
             foreach ($choice as $item) {
-                echo "<li>" . $item;
+                echo "<li>" . "$". $item;
             }
             ?>
         </div>
