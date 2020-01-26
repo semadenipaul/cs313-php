9c32c93ce0fb45f22b038a1f8fa4d1390a547f6e
Your Name <sem16002@byui.edu>
2020-01-25 18:32:34 -0700

    assignment 3 forty-nine commit

--- web/week03/assign_03/confirmation.php
+++ web/week03/assign_03/confirmation.php
@@ -37,12 +37,12 @@
 
         <div class="item2">
             <?php
+            $value = $_GET["validate"];
+            $value2 = $_GET["reset"];
             if ($value2 == 1) {
                 exit("Your order has been cancelled. Click on this link to return to homepage: https://thawing-woodland-23025.herokuapp.com/week03/assign_03/assign_03.html");
             }
-
-            $value = $_GET["validate"];
-            $value2 = $_GET["reset"];
+            
             echo "Your order has been CONFIRMED! <br>";
             echo $_SESSION["item"] . "</b>";
             echo "<b>Your total is: " . $_SESSION["total"] . "</b>";
