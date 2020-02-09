de4b89c26e2ad429adf4b464bd4c39b9710663af
Your Name <sem16002@byui.edu>
2020-02-08 20:04:25 -0700

    adding national park images

--- web/week04/assign_04/home_page.php
+++ web/week04/assign_04/home_page.php
@@ -26,18 +26,18 @@ session_start();
                 <li><a href="">About Us</a></li>
             </ul>
         </div>
-        <div class="item3" id="cf">
-            <img class="image1" src="alaska.jpg" />
-            <img class="image2" src="yosemite.jpg" />
-            <img class="image3" src="arcadia.jpg" />
-            <img class="image4" src="yellowstone.gif" />
-            <img class="image5" src="arches.jpg" />
-            <img class="image6" src="teton.jpg" />
-            <img class="image7" src="canyonlands.jpg" />
-            <img class="image8" src="smokymtn.jpg" />
-            <img class="image9" src="everglades.jpg" />
-            <img class="image10" src="hawaii.jpg" />
-            <img class="image11" src="glacier.jpg" />
+        <div class="item3">
+            <img id="image1" src="alaska.jpg" />
+            <img id="image2" src="yosemite.jpg" />
+            <img id="image3" src="arcadia.jpg" />
+            <img id="image4" src="yellowstone.gif" />
+            <img id="image5" src="arches.jpg" />
+            <img id="image6" src="teton.jpg" />
+            <img id="image7" src="canyonlands.jpg" />
+            <img id="image8" src="smokymtn.jpg" />
+            <img id="image9" src="everglades.jpg" />
+            <img id="image10" src="hawaii.jpg" />
+            <img id="image11" src="glacier.jpg" />
         </div>
         <div class="item4"></div>
         <div class="item5"></div>
