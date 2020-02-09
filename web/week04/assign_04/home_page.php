624564202cd96304413887a80b7150825681c7c8
Your Name <sem16002@byui.edu>
2020-02-08 19:38:04 -0700

    background image adjustment

--- web/week04/assign_04/home_page.php
+++ web/week04/assign_04/home_page.php
@@ -26,7 +26,19 @@ session_start();
                 <li><a href="">About Us</a></li>
             </ul>
         </div>
-        <div class="item3"></div>
+        <div class="item3">
+            <img class="image1" src="/images/alaska.jpg" />
+            <img class="image2" src="/images/yosemite.jpg" />
+            <img class="image3" src="/images/arcadia.jpg" />
+            <img class="image4" src="/images/yellowstone.jpg" />
+            <img class="image5" src="/images/arches.jpg" />
+            <img class="image6" src="/images/teton.jpg" />
+            <img class="image7" src="/images/canyonlands.jpg" />
+            <img class="image8" src="/images/smokymtn.jpg" />
+            <img class="image9" src="/images/everglades.jpg" />
+            <img class="image10" src="/images/hawaii.jpg" />
+            <img class="image11" src="/images/glacier.jpg" />
+        </div>
         <div class="item4"></div>
         <div class="item5"></div>
     </form>
