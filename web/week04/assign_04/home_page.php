c286787428f72c76b56ed8575cdf2287e68da20f
Your Name <sem16002@byui.edu>
2020-02-11 13:50:05 -0700

    background images for home_page

--- web/week04/assign_04/home_page.php
+++ web/week04/assign_04/home_page.php
@@ -26,21 +26,23 @@ session_start();
                 <li><a href="">About Us</a></li>
             </ul>
         </div>
-        <div class="item3"> <img id="image1" src="alaska.jpg" /></div>
-        <div class="item4"> <img id="image2" src="yosemite.jpg" /></div>
-        <div class="item5"> <img id="image3" src="arcadia.jpg" /></div>
-        <div class="item6"> <img id="image4" src="yellowstone.gif" /></div>
-        <div class="item7"><img id="image5" src="arches.jpg" /></div>
-        <div class="item8"><img id="image6" src="teton.jpg" /></div>
-        <div class="item9"><img id="image7" src="canyonlands.jpg" /></div>
-        <div class="item10"><img id="image8" src="smokymtn.jpg" /></div>
-        <div class="item11"><img id="image9" src="everglades.jpg" /></div>
-        <div class="item12"><img id="image10" src="hawaii.jpg" /></div>
-        <div class="item13"><img id="image11" src="glacier.jpg" /></div>
-
-        <div class="item14"></div>
-        <div class="item15"></div>
+        <div class="item3" id="stage">
+            <a href="alaska.jpg"><img id="image1" src="alaska.jpg" /></a>
+            <a href="yosemite.jpg"><img id="image2" src="yosemite.jpg" /></a>
+            <a href="arcadia.jpg"><img id="image3" src="arcadia.jpg" /></a>
+            <a href="yellowstone.gif"><img id="image4" src="yellowstone.gif" /></a>
+            <a href="arches.jpg"><img id="image5" src="arches.jpg" /></a>
+            <a href="teton.jpg"><img id="image6" src="teton.jpg" /></a>
+            <a href="canyonlands.jpg"><img id="image7" src="canyonlands.jpg" /></a>
+            <a href="smokymtn.jpg"><img id="image8" src="smokymtn.jpg" /></a>
+            <a href="everglades.jpg"><img id="image9" src="everglades.jpg" /></a>
+            <a href="hawaii.jpg"><img id="image10" src="hawaii.jpg" /></a>
+            <a href="glacier.jpg"><img id="image11" src="glacier.jpg" /></a>
+        </div>
+        <div class="item4"></div>
+        <div class="item5"></div>
     </form>
+    <script src="home_page.js"></script>
 </body>
 
 </html>
