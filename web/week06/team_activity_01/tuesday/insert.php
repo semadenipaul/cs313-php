23cb98028eb5c4f633acb53224849a33759fa623
Your Name <sem16002@byui.edu>
2020-02-11 15:55:44 -0700

    tuesday  class practice

---
+++ web/week06/team_activity_01/tuesday/insert.php
@@ -0,0 +1,34 @@
+<?php
+// retrieve POST data from the other page
+$first = $_POST['first'];
+$last = $_POST['last'];
+$food = $_POST['food'];
+
+
+require("dbConnect.php");
+$db = get_db();
+
+try
+{
+	$query = 'INSERT INTO w6_user (first_name, last_name, food_type) VALUES (:first, :last, :food)';
+	$statement = $db->prepare($query);
+	$statement->bindValue(':first', $first);
+	$statement->bindValue(':last', $last);
+	$statement->bindValue(':food', $food);
+	$statement->execute();
+
+	$userId = $db->lastInsertId('w6_user_id_seq');
+	// insert into database
+	
+	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
+	// get id of last inserted row - save in $userId
+}
+catch (Exception $ex)
+{
+	echo "Error with DB. Details: $ex";
+	die();
+}
+header("Location: display.php/?personId=$userId"); //Sends the data to display.php
+
+die(); 
+?>
