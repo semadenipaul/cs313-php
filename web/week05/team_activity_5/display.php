5beffdd51bca4a308e5909952558c7477e6e1da0
Your Name <sem16002@byui.edu>
2020-02-12 17:01:39 -0700

    team activity week 6

--- web/week05/team_activity_5/display.php
+++ web/week05/team_activity_5/display.php
@@ -7,25 +7,28 @@ $db = get_db();
    <div class="container">
       <?php
       // retrieve url parameter
-      $personId = $_GET['personId'];
+      $scriptureId = $_GET['scriptureId'];
 
       // execute query to pull up data from that id
-      $statement = $db->prepare('SELECT * FROM w6_user WHERE Id = :personId');
-      $statement->bindValue(':personId', $personId);
+      $statement = $db->prepare('SELECT * FROM Scriptures WHERE Id = :scriptureId');
+      $statement->bindValue(':scriptureId', $scriptureId);
       $statement->execute();
 
       while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
          $id = $row['id'];
-         $first = $row['first_name'];
-         $last = $row['last_name'];
-         $food_id = $row['food_type'];
+         $book = $row['book'];
+         $chapter = $row['chapter'];
+         $verse = $row['verse'];
+         $content = $row['content'];
+         $topic_id = $row['topic[]'];
 
-         $foods = $db->prepare("SELECT * FROM w6_food WHERE Id = $food_id");
-               $foods->execute();
-               while ($frow = $foods->fetch(PDO::FETCH_ASSOC)){
-                  $food = $frow['food'];
+         $topic = $db->prepare("SELECT * FROM scripture_topic WHERE Id = $topic_id");
+               $topic->execute();
+               while ($frow = $topic->fetch(PDO::FETCH_ASSOC)){
+                  $topic = $frow['topic[]'];
                }
-         echo "<h1>$first $last's favorite food is $food</h1>";
+         echo "<h1>$book $chapter :$verse</h1>
+         <br> <p>$content</p> <br> <p>Topics: $topic</p>";
       }
       // execute another query to get food data
       // display name and favorite food
