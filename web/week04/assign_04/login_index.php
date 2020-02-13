fb59dd9b0e8c19b73a7353ee9dadfd171984e506
Your Name <sem16002@byui.edu>
2020-02-13 16:38:28 -0700

    created login_index.php and it's style sheet

--- web/week04/assign_04/login_index.php
+++ web/week04/assign_04/login_index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css?family=Cabin+Sketch&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="index_style_sheet.css">
+    <link rel="stylesheet" href="index_style_sheets.css">
     <title>| Welcome</title>
 </head>
 
