96c679c2d38dc0260a62543c8790e9f48e0c8be5
Your Name <sem16002@byui.edu>
2020-02-15 14:51:19 -0700

    added dropdown menu for U.S. States

--- web/week04/assign_04/login_index.php
+++ web/week04/assign_04/login_index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-if(isset($_POST['Submit'])) {
+if (isset($_POST['Submit'])) {
     $_SESSION['username'] = $_POST['username'];
     $_SESSION['password'] = $_POST['password'];
 }
@@ -33,7 +33,66 @@ if(isset($_POST['Submit'])) {
         </div>
         <div class="item3">
             <p>What words describe the outdoor vacation you seek?</p>
-            <input type="text" class="keywords" placeholder="Keywords" name="keywords"><br><br>
+            <p>Which state would you like to visit?
+                <select id="states">
+                    <option value="Alabama">Alabama</option>
+                    <option value="Alaska">Alaska</option>
+                    <option value="Arizona">Arizona</option>
+                    <option value="Arkansas">Arkansas</option>
+                    <option value="California">California</option>
+                    <option value="Colorado">Colorado</option>
+                    <option value="Connecticut">Connecticut</option>
+                    <option value="Delaware">Delaware</option>
+                    <option value="Florida">Florida</option>
+                    <option value="Georgia">Georgia</option>
+                    <option value="Hawaii">Hawaii</option>
+                    <option value="Idaho">Idaho</option>
+                    <option value="Illinois">Illinois</option>
+                    <option value="Indiana">Indiana</option>
+                    <option value="Iowa">Iowa</option>
+                    <option value="Kansas">Kansas</option>
+                    <option value="Kentucky">Kentucky</option>
+                    <option value="Louisiana">Louisiana</option>
+                    <option value="Maine">Maine</option>
+                    <option value="Maryland">Maryland</option>
+                    <option value="Massachusetts">Massachusetts</option>
+                    <option value="Michigan">Michigan</option>
+                    <option value="Minnesota">Minnesota</option>
+                    <option value="Mississippi">Mississippi</option>
+                    <option value="Missouri">Missouri</option>
+                    <option value="Montana">Montana</option>
+                    <option value="Nebraska">Nebraska</option>
+                    <option value="Nevada">Nevada</option>
+                    <option value="New Hampshire">New Hampshire</option>
+                    <option value="New Jersey">New Jersey</option>
+                    <option value="New Mexico">New Mexico</option>
+                    <option value="New York">New York</option>
+                    <option value="North Carolina">North Carolina</option>
+                    <option value="North Dakota">North Dakota</option>
+                    <option value="Ohio">Ohio</option>
+                    <option value="Oklahoma">Oklahoma</option>
+                    <option value="Oregon">Oregon</option>
+                    <option value="Pennsylvania">Pennsylvania</option>
+                    <option value="Rhode Island">Rhode Island</option>
+                    <option value="South Carolina">South Carolina</option>
+                    <option value="South Dakota">South Dakota</option>
+                    <option value="Tennessee">Tennessee</option>
+                    <option value="Texas">Texas</option>
+                    <option value="Utah">Utah</option>
+                    <option value="Vermont">Vermont</option>
+                    <option value="Virginia">Virginia</option>
+                    <option value="Washington">Washington</option>
+                    <option value="West Virginia">West Virginia</option>
+                    <option value="Wisconsin">Wisconsin</option>
+                    <option value="Wyoming">Wyoming</option>
+                </select>
+            </p>
+            <p>Re-Enter Username: <input type="text" name="username2" /></p>
+            <p>Password: <input type="text" name="password" /></p>
+            <p>Re-Enter Password: <input type="text" name="password2" /></p>
+            <p>Email: <input type="text" name="email" /></p>
+            <p>City: <input type="text" name="city" /></p>
+            <p>State: <input type="text" name="state" /></p><br><br>
             <button class="submit_button" type="submit">Show Me My Vacation Destination!</button>
         </div>
         <div class="item4"></div>
