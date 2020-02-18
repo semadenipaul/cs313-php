7c77df61474b1eee5cddd442f09fda483566a075
Your Name <sem16002@byui.edu>
2020-02-18 15:41:50 -0700

    adding vacation_time options

--- web/week04/assign_04/login_index.php
+++ web/week04/assign_04/login_index.php
@@ -117,6 +117,20 @@ $db = get_db();
                     ?>
                 </select>
             </p>
+            <p>How many days do you want to spend?
+            <select id="vacation_time" name="vacation_time">
+                    <?php
+                    $statement = $db->prepare("SELECT DISTINCT vacation_time FROM national_parks ORDER BY vacation_time");
+                    $statement->execute();
+
+                    while ($vrow = $statement->fetch(PDO::FETCH_ASSOC)) {
+                        $id = $vrow['id'];
+                        $vacation_time = $vrow['vacation_time'];
+                        echo "<option value='$id'>$vacation_time</option>";
+                    }
+                    ?>
+                </select>
+            </p>
             <br><br>
             <button class="submit_button" type="submit">Show Me My Vacation Destination!</button>
         </div>
