932822ea98b298cb964cf9380be6f1de158b6de6
Your Name <sem16002@byui.edu>
2020-02-20 16:00:52 -0700

    team activity

--- web/week07/team_activity/signUp.php
+++ web/week07/team_activity/signUp.php
@@ -6,25 +6,25 @@
   }
 
   else {?>
-    <!DOCTYPE html>
-            <head>
-                <meta charset='utf-8'>
-                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-                <title>Sign up</title>
-                <meta name='viewport' content='width=device-width, initial-scale=1'>
-            </head>
-            <body>
-                <form action='' method='POST'>
-                    <label for='username'>New username</label>
-                    <input type='text' id='username' name='username'/>
-                    <br/>
-                    <label for='password'>New password</label>
-                    <input type='password' id='password' name='password'/>
-                    <br/>
-                    <input type='submit' id='submit' value='Create account'/>
-                </form>
-            </body>
-        </html>
-  <?php }
+<!DOCTYPE html>
+        <head>
+            <meta charset='utf-8'>
+            <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+            <title>Sign up</title>
+            <meta name='viewport' content='width=device-width, initial-scale=1'>
+        </head>
+        <body>
+            <form action='' method='POST'>
+                <label for='username'>New username</label>
+                <input type='text' id='username' name='username'/>
+                <br/>
+                <label for='password'>New password</label>
+                <input type='password' id='password' name='password'/>
+                <br/>
+                <input type='submit' id='submit' name='submit' value='Create account'/>
+            </form>
+        </body>
+    </html>
+<?php }
 
 ?>
