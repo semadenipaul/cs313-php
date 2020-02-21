c749e5dfe69f4a04a3095a38144bc93c06db3e73
Your Name <sem16002@byui.edu>
2020-02-21 16:26:01 -0700

    using header instead of action

--- web/week04/assign_04/compare_index.php
+++ web/week04/assign_04/compare_index.php
@@ -1,48 +1,47 @@
 <?php
 session_start();
 
-if (isset($_POST['Submit'])) {
-$username = $_SESSION['username'];
-$password = $_SESSION['password'];
-}
-$state = $_POST['state'];
-$nature1 = $_POST['nature1'];
-$nature2 = $_POST['nature2'];
-$activities1 = $_POST['activities1'];
-$activities2 = $_POST['activities2'];
-$activities3 = $_POST['activities3'];
-$vacation_time = $_POST['vacation_time'];
-
-// retrieve POST data from the other page
-
 require("db_connect.php");
 $db = get_db();
 
-try
-{
-	$query = 'INSERT INTO national_parks_selected (us_state, nature1, nature2, activities1, activities2, activities3, vacation_time) VALUES (:state, :nature1, :nature2, :activities1, :activities2, :activities3, :vacation_time)';
-	$statement = $db->prepare($query);
-	$statement->bindValue(':state', $state);
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+    $password = $_SESSION['password'];
+}
+
+if (isset($_POST['Submit'])) {
+    $state = $_POST['state'];
+    $nature1 = $_POST['nature1'];
+    $nature2 = $_POST['nature2'];
+    $activities1 = $_POST['activities1'];
+    $activities2 = $_POST['activities2'];
+    $activities3 = $_POST['activities3'];
+    $vacation_time = $_POST['vacation_time'];
+}
+// retrieve POST data from the other page
+
+try {
+    $query = 'INSERT INTO national_parks_selected (us_state, nature1, nature2, activities1, activities2, activities3, vacation_time) VALUES (:state, :nature1, :nature2, :activities1, :activities2, :activities3, :vacation_time)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':state', $state);
     $statement->bindValue(':nature1', $nature1);
     $statement->bindValue(':nature2', $nature2);
     $statement->bindValue(':activities1', $activities1);
     $statement->bindValue(':activities2', $activities2);
     $statement->bindValue(':activities3', $activities3);
     $statement->bindValue(':vacation_time', $vacation_time);
-	$statement->execute();
+    $statement->execute();
 
-	$userId = $db->lastInsertId('national_parks_selected_id_seq');
-	// insert into database
-	
-	// SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
-	// get id of last inserted row - save in $userId
-}
-catch (Exception $ex)
-{
-	echo "Error with DB. Details: $ex";
-	die();
+    $userId = $db->lastInsertId('national_parks_selected_id_seq');
+    // insert into database
+
+    // SELECT c.relname FROM pg_class c WHERE c.relkind = 'S';   -- display all sequences
+    // get id of last inserted row - save in $userId
+} catch (Exception $ex) {
+    echo "Error with DB. Details: $ex";
+    die();
 }
 header("Location: display_national_park.php/?stateParkId=$stateParkId"); //Sends the data to display.php
 
-die(); 
-?>
+die();
