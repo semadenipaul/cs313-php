be0b1ee535f95daf3a9228c66f0bdb5b42757b3d
Your Name <sem16002@byui.edu>
2020-02-22 20:29:45 -0700

    displaying the pervious trips page

--- web/week04/assign_04/previous_trips.js
+++ web/week04/assign_04/previous_trips.js
@@ -1,4 +1,7 @@
 function createTextArea() {
     var textBox = document.createElement("TEXTAREA");
+    var text = document.createTextNode("");
+
+    textBox.appendChild(text);
     document.body.appendChild(textBox);
 }
