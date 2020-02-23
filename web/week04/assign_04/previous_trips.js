e61309f25eb6c0b9534e3934ef943138053dad8f
Your Name <sem16002@byui.edu>
2020-02-22 21:10:26 -0700

    displaying the pervious trips page

--- web/week04/assign_04/previous_trips.js
+++ web/week04/assign_04/previous_trips.js
@@ -1,6 +1,3 @@
 function myFunction() {
-    var x = document.createElement("TEXTAREA");
-    var t = document.createTextNode("At w3schools.com you will learn how to make a website.");
-    x.appendChild(t);
-    document.body.appendChild(x);
+    document.getElementById('review').className="show";
 }
--- web/week04/assign_04/previous_trips.php
+++ web/week04/assign_04/previous_trips.php
@@ -38,6 +38,7 @@ $db = get_db();
         </div>
         <div class="item3">
             <?php 
+            echo "Username = " . $username . " :)";
             $query = "SELECT * FROM person WHERE user_name = '$username'";
             $usr = $db->prepare($query);
             $usr->execute();
@@ -51,6 +52,7 @@ $db = get_db();
             ?>
             <p>Write a review for the National Parks you have visited!</p>
             <button onclick="myFunction()">Write a Review</button><br>
+            <textarea name="review" id="review" cols="100" rows="50"></textarea>
         </div>
         <div class="item4"></div>
     </form>
